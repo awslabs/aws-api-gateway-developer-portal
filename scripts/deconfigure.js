ce71fbb4d0925912b8fa629f8d4233a9d5575e15
Andrews <brett.j.andrews@gmail.com>
2016-11-30 19:25:36 -0800

    make deconfigure more strict

--- scripts/deconfigure.js
+++ scripts/deconfigure.js
@@ -14,10 +14,9 @@ const config = packageJson.config
 
 module.exports = function() {
   modifyApigClient(config.apiGatewayApiId, config.primaryAwsRegion)
-  modifyDevPortalJs(config.cognitoIdentityPoolId, config.primaryAwsRegion, config.cognitoRegion, config.cognitoUserPoolId, config.cognitoClientId)
+  modifyDevPortalAwsService(config.cognitoIdentityPoolId, config.primaryAwsRegion, config.cognitoRegion, config.cognitoUserPoolId, config.cognitoClientId)
   modifySwaggerFile(config.accountId, config.primaryAwsRegion, config.apiGatewayApiName)
   modifyExpressServer(config.siteS3Bucket, config.primaryAwsRegion)
-  modifyCloudFormation(config.cognitoIdentityPoolId)
   modifyPackageFile(config)
   modifyUiPackageFile(config.siteS3Bucket, config.primaryAwsRegion)
 }
@@ -25,29 +24,29 @@ module.exports = function() {
 function modifyApigClient(apiGatewayApiId, primaryAwsRegion) {
     const apigClientPath = `${rootDir}/dev-portal/public/apigateway-js-sdk/apigClient.js`
     const apigClient = fs.readFileSync(apigClientPath, 'utf8')
-    const apiGatewayApiIdRegex = new RegExp(apiGatewayApiId, 'g')
-    const primaryAwsRegionRegex = new RegExp(primaryAwsRegion, 'g')
+    const invokeUrlRegex = new RegExp(`var invokeUrl = 'https://${apiGatewayApiId}.execute-api.${primaryAwsRegion}.amazonaws.com/prod';`, 'g')
+    const primaryAwsRegionRegex = new RegExp(`config.region = '${primaryAwsRegion}';`, 'g')
     const apigClientModified = apigClient
-        .replace(apiGatewayApiIdRegex, 'YOUR_API_GATEWAY_API_ID')
-        .replace(primaryAwsRegionRegex, 'YOUR_PRIMARY_AWS_REGION')
+      .replace(invokeUrlRegex, 'var invokeUrl = \'https://YOUR_API_GATEWAY_API_ID.execute-api.YOUR_PRIMARY_AWS_REGION.amazonaws.com/prod\';')
+      .replace(primaryAwsRegionRegex, 'config.region = 'YOUR_PRIMARY_AWS_REGION';')
 
     fs.writeFileSync(apigClientPath, apigClientModified, 'utf8')
 }
 
-function modifyDevPortalJs(cognitoIdentityPoolId, primaryAwsRegion, cognitoRegion, cognitoUserPoolId, cognitoClientId) {
+function modifyDevPortalAwsService(cognitoIdentityPoolId, primaryAwsRegion, cognitoRegion, cognitoUserPoolId, cognitoClientId) {
     const htmlPath = `${rootDir}/dev-portal/src/services/aws.js`
     const html = fs.readFileSync(htmlPath, 'utf8')
-    const cognitoIdentityPoolIdRegex = new RegExp(cognitoIdentityPoolId, 'g')
-    const cognitoRegionRegex = new RegExp(`const cognitoRegion = '${cognitoRegion}'`, 'g')
-    const primaryAwsRegionRegex = new RegExp(`const awsRegion = '${primaryAwsRegion}'`, 'g')
-    const cognitoUserPoolIdRegex = new RegExp(cognitoUserPoolId, 'g')
-    const cognitoClientIdRegex = new RegExp(cognitoClientId, 'g')
+    const cognitoIdentityPoolIdRegex = new RegExp(`export const cognitoIdentityPoolId = '${cognitoIdentityPoolId}'`, 'g')
+    const cognitoRegionRegex = new RegExp(`export const cognitoRegion = '${cognitoRegion}'`, 'g')
+    const primaryAwsRegionRegex = new RegExp(`export const awsRegion = '${primaryAwsRegion}'`, 'g')
+    const cognitoUserPoolIdRegex = new RegExp(`export const cognitoUserPoolId = '${cognitoUserPoolId}'`, 'g')
+    const cognitoClientIdRegex = new RegExp(`export const cognitoClientId = '${cognitoClientId}'`, 'g')
     const htmlModified = html
-        .replace(cognitoIdentityPoolIdRegex, 'YOUR_COGNITO_IDENTITY_POOL_ID')
-        .replace(cognitoUserPoolIdRegex, 'YOUR_COGNITO_USER_POOL_ID')
-        .replace(cognitoClientIdRegex, 'YOUR_COGNITO_CLIENT_ID')
-        .replace(primaryAwsRegionRegex, `const awsRegion = 'YOUR_PRIMARY_AWS_REGION'`)
-        .replace(cognitoRegionRegex, `const cognitoRegion = 'YOUR_COGNITO_REGION'`)
+      .replace(cognitoIdentityPoolIdRegex, 'export const cognitoIdentityPoolId = \'YOUR_COGNITO_IDENTITY_POOL_ID\'')
+      .replace(cognitoUserPoolIdRegex, 'export const cognitoUserPoolId = \'YOUR_COGNITO_USER_POOL_ID\'')
+      .replace(cognitoClientIdRegex, 'export const cognitoClientId = \'YOUR_COGNITO_CLIENT_ID\'')
+      .replace(primaryAwsRegionRegex, `export const awsRegion = 'YOUR_PRIMARY_AWS_REGION'`)
+      .replace(cognitoRegionRegex, `export const cognitoRegion = 'YOUR_COGNITO_REGION'`)
 
     fs.writeFileSync(htmlPath, htmlModified, 'utf8')
 }
@@ -56,12 +55,11 @@ function modifySwaggerFile(accountId, primaryAwsRegion, apiGatewayApiName) {
     const swaggerDefinitionPath = `${rootDir}/lambdas/backend/dev-portal-express-proxy-api.yaml`
     const swaggerDefinition = fs.readFileSync(swaggerDefinitionPath, 'utf8')
     const accountIdRegex = new RegExp(accountId, 'g')
-    const apiGatewayApiNameRegex = new RegExp(apiGatewayApiName, 'g')
-    const primaryAwsRegionRegex = new RegExp(primaryAwsRegion, 'g')
+    const apiGatewayApiNameRegex = new RegExp(`title: ${apiGatewayApiName}`, 'g')
+    const lambdaArnRegex = new RegExp(`uri: arn:aws:apigateway:${primaryAwsRegion}:lambda:path/2015-03-31/functions/arn:aws:lambda:${primaryAwsRegion}:${accountId}`, 'g')
     const simpleProxyApiModified = swaggerDefinition
-        .replace(accountIdRegex, 'YOUR_ACCOUNT_ID')
-        .replace(apiGatewayApiNameRegex, 'YOUR_API_GATEWAY_API_NAME')
-        .replace(primaryAwsRegionRegex, 'YOUR_PRIMARY_AWS_REGION')
+      .replace(apiGatewayApiNameRegex, 'title: YOUR_API_GATEWAY_API_NAME')
+      .replace(lambdaArnRegex, 'uri: arn:aws:apigateway:YOUR_PRIMARY_AWS_REGION:lambda:path/2015-03-31/functions/arn:aws:lambda:YOUR_PRIMARY_AWS_REGION:YOUR_ACCOUNT_ID')
 
     fs.writeFileSync(swaggerDefinitionPath, simpleProxyApiModified, 'utf8')
 }
@@ -69,49 +67,38 @@ function modifySwaggerFile(accountId, primaryAwsRegion, apiGatewayApiName) {
 function modifyExpressServer(siteS3Bucket, primaryAwsRegion) {
     const expressServerPath = `${rootDir}/lambdas/backend/express-server.js`
     const expressServer = fs.readFileSync(expressServerPath, 'utf8')
-    const siteS3BucketRegex = new RegExp(siteS3Bucket, 'g')
-    const primaryAwsRegionRegex = new RegExp(primaryAwsRegion, 'g')
+    const domainRegex = new RegExp(`const domain = '${siteS3Bucket}.s3-website-${primaryAwsRegion}.amazonaws.com'`, 'g')
+
     const expressServerModified = expressServer
-        .replace(siteS3BucketRegex, 'YOUR_CLIENT_BUCKET_NAME')
-        .replace(primaryAwsRegionRegex, 'YOUR_PRIMARY_AWS_REGION')
+      .replace(domainRegex, 'const domain = \'YOUR_CLIENT_BUCKET_NAME.s3-website-YOUR_PRIMARY_AWS_REGION.amazonaws.com\'')
 
     fs.writeFileSync(expressServerPath, expressServerModified, 'utf8')
-}
-
-function modifyCloudFormation(cognitoIdentityPoolId) {
-    const cloudFormationPath = `${rootDir}/cloudformation/base.yaml`
-    const cloudFormation = fs.readFileSync(cloudFormationPath, 'utf8')
-    const cognitoIdentityPoolIdRegex = new RegExp(cognitoIdentityPoolId, 'g')
-    const cloudFormationModified = cloudFormation
-        .replace(cognitoIdentityPoolIdRegex, 'YOUR_COGNITO_IDENTITY_POOL_ID')
-
-    fs.writeFileSync(cloudFormationPath, cloudFormationModified, 'utf8')
 }
 
 function modifyPackageFile(config) {
     const packageJsonPath = `${rootDir}/package.json`
     const packageJson = fs.readFileSync(packageJsonPath, 'utf8')
     const artifactsS3BucketRegex = new RegExp(`"artifactsS3Bucket": "${config.artifactsS3Bucket}"`, 'g')
-    const siteS3BucketRegex = new RegExp(config.siteS3Bucket, 'g')
-    const apiGatewayApiNameRegex = new RegExp(config.apiGatewayApiName, 'g')
-    const accountIdRegex = new RegExp(config.accountId, 'g')
-    const cloudFormationStackNameRegex = new RegExp(config.cloudFormationStackName, 'g')
+    const siteS3BucketRegex = new RegExp(`"siteS3Bucket": "${config.siteS3Bucket}"`, 'g')
+    const apiGatewayApiNameRegex = new RegExp(`"apiGatewayApiName": "${config.apiGatewayApiName}"`, 'g')
+    const accountIdRegex = new RegExp(`"accountId": "${config.accountId}"`, 'g')
+    const cloudFormationStackNameRegex = new RegExp(`"cloudFormationStackName": "${config.cloudFormationStackName}"`, 'g')
     const primaryAwsRegionRegex = new RegExp(`"primaryAwsRegion": "${config.primaryAwsRegion}"`, 'g')
-    const apiGatewayApiIdRegex = new RegExp(config.apiGatewayApiId, 'g')
+    const apiGatewayApiIdRegex = new RegExp(`"apiGatewayApiId": "${config.apiGatewayApiId}"`, 'g')
     const cognitoRegionRegex = new RegExp(`"cognitoRegion": "${config.cognitoRegion}"`, 'g')
-    const cognitoUserPoolIdRegex = new RegExp(config.cognitoUserPoolId, 'g')
-    const cognitoClientIdRegex = new RegExp(config.cognitoClientId, 'g')
-    const cognitoIdentityPoolIdRegex = new RegExp(config.cognitoIdentityPoolId, 'g')
+    const cognitoUserPoolIdRegex = new RegExp(`"cognitoUserPoolId": "${config.cognitoUserPoolId}"`, 'g')
+    const cognitoClientIdRegex = new RegExp(`"cognitoClientId": "${config.cognitoClientId}"`, 'g')
+    const cognitoIdentityPoolIdRegex = new RegExp(`"cognitoIdentityPoolId": "${config.cognitoIdentityPoolId}"`, 'g')
     const packageJsonModified = packageJson
       .replace(artifactsS3BucketRegex, '"artifactsS3Bucket": "YOUR_ARTIFACTS_BUCKET_NAME"')
-      .replace(siteS3BucketRegex, 'YOUR_CLIENT_BUCKET_NAME')
-      .replace(apiGatewayApiNameRegex, 'YOUR_API_GATEWAY_API_NAME')
-      .replace(accountIdRegex, 'YOUR_ACCOUNT_ID')
-      .replace(cloudFormationStackNameRegex, 'YOUR_CLOUDFORMATION_STACK_NAME')
-      .replace(apiGatewayApiIdRegex, 'YOUR_API_GATEWAY_API_ID')
-      .replace(cognitoUserPoolIdRegex, 'YOUR_COGNITO_USER_POOL_ID')
-      .replace(cognitoClientIdRegex, 'YOUR_COGNITO_CLIENT_ID')
-      .replace(cognitoIdentityPoolIdRegex, 'YOUR_COGNITO_IDENTITY_POOL_ID')
+      .replace(siteS3BucketRegex, '"siteS3Bucket": "YOUR_CLIENT_BUCKET_NAME"')
+      .replace(apiGatewayApiNameRegex, '"apiGatewayApiName": "YOUR_API_GATEWAY_API_NAME"')
+      .replace(accountIdRegex, '"accountId": "YOUR_ACCOUNT_ID"')
+      .replace(cloudFormationStackNameRegex, '"cloudFormationStackName": "YOUR_CLOUDFORMATION_STACK_NAME"')
+      .replace(apiGatewayApiIdRegex, '"apiGatewayApiId": "YOUR_API_GATEWAY_API_ID"')
+      .replace(cognitoUserPoolIdRegex, '"cognitoUserPoolId": "YOUR_COGNITO_USER_POOL_ID"')
+      .replace(cognitoClientIdRegex, '"cognitoClientId": "YOUR_COGNITO_CLIENT_ID"')
+      .replace(cognitoIdentityPoolIdRegex, '"cognitoIdentityPoolId": "YOUR_COGNITO_IDENTITY_POOL_ID"')
       .replace(primaryAwsRegionRegex, '"primaryAwsRegion": "YOUR_PRIMARY_AWS_REGION"')
       .replace(cognitoRegionRegex, '"cognitoRegion": "YOUR_COGNITO_REGION"')
 
@@ -121,11 +108,10 @@ function modifyPackageFile(config) {
 function modifyUiPackageFile(siteS3Bucket, primaryAwsRegion) {
     const packageJsonPath = `${rootDir}/dev-portal/package.json`
     const packageJson = fs.readFileSync(packageJsonPath, 'utf8')
-    const primaryAwsRegionRegex = new RegExp(primaryAwsRegion, 'g')
-    const siteS3BucketRegex = new RegExp(siteS3Bucket, 'g')
+    const homePageRegex = new RegExp(`"homepage": "http://${siteS3Bucket}.s3-website-${primaryAwsRegion}.amazonaws.com/"`, 'g')
+
     const packageJsonModified = packageJson
-      .replace(primaryAwsRegionRegex, 'YOUR_PRIMARY_AWS_REGION')
-      .replace(siteS3BucketRegex, 'YOUR_CLIENT_BUCKET_NAME')
+      .replace(homePageRegex, '"homepage": "http://YOUR_CLIENT_BUCKET_NAME.s3-website-YOUR_PRIMARY_AWS_REGION.amazonaws.com/"')
 
     fs.writeFileSync(packageJsonPath, packageJsonModified, 'utf8')
 }
