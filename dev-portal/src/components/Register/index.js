3804e7902428e3b46fb962901b27be700f15803c
Andrews <brett.j.andrews@gmail.com>
2016-11-30 16:34:53 -0800

    add close buttons to signin and register modals

--- dev-portal/src/components/Register/index.js
+++ dev-portal/src/components/Register/index.js
@@ -4,19 +4,18 @@ import { Redirect } from 'react-router'
 import { register } from '../../services/self'
 
  export default class Register extends React.Component {
-  constructor(props){
-    super(props)
-
-    this.state = {
-      isSubmitting: false,
-      signedIn: false,
-      errorMessage: ''
-    }
-
-    this.handleRegister = this.handleRegister.bind(this);
+  state = {
+    isSubmitting: false,
+    signedIn: false,
+    errorMessage: '',
+    isOpen: false
   }
 
-  handleRegister(event, serializedForm) {
+  open = () => this.setState({ isSubmitting: false, errorMessage: '', isOpen: true })
+  close = () => this.setState({ isOpen: false })
+  handleRegister = (...args) => this._handleRegister(...args)
+
+  _handleRegister(event, serializedForm) {
     event.preventDefault()
     this.setState({isSubmitting: true})
     register(serializedForm.email, serializedForm.password)
@@ -25,20 +24,30 @@ import { register } from '../../services/self'
   }
 
   render() {
-    console.log('this.state.signedIn', this.state.signedIn)
+    const { isOpen } = this.state
+
     return this.state.signedIn ? <Redirect to='/apis' /> : (
-      <Modal size='small' trigger={<Button secondary fluid>Register</Button>}>
+      <Modal
+        size='small'
+        open={isOpen}
+        onOpen={this.open}
+        onClose={this.close}
+        trigger={<Button secondary fluid>Register</Button>}
+      >
         <Modal.Header>Register</Modal.Header>
         <Modal.Content>
           <Modal.Description>
             <Form onSubmit={this.handleRegister} error={!!this.state.errorMessage} loading={this.state.isSubmitting}>
               <Form.Input label='Email' name='email' />
               <Form.Input type='password' label='Password' name='password' autoComplete='false' />
-              <Button primary type='submit'>Register</Button>
               <Message error content={this.state.errorMessage} />
             </Form>
           </Modal.Description>
         </Modal.Content>
+        <Modal.Actions>
+          <Button onClick={this.close}>Close</Button>
+          <Button primary onClick={this.handleRegister}>Register</Button>
+        </Modal.Actions>
       </Modal>)
     }
 }
--- dev-portal/src/components/SignIn/index.js
+++ dev-portal/src/components/SignIn/index.js
@@ -4,19 +4,17 @@ import { Redirect } from 'react-router'
 import { login } from '../../services/self'
 
  export default class SignIn extends React.Component {
-  constructor(props){
-    super(props)
+   state = {
+     isSubmitting: false,
+     signedIn: false,
+     errorMessage: '',
+     isOpen: false
+   }
+   open = () => this.setState({ isSubmitting: false, errorMessage: '', isOpen: true })
+   close = () => this.setState({ isOpen: false })
+   handleLogin = (...args) => this._handleLogin(...args)
 
-    this.state = {
-      isSubmitting: false,
-      signedIn: false,
-      errorMessage: ''
-    }
-
-    this.handleLogin = this.handleLogin.bind(this)
-  }
-
-  handleLogin(event, serializedForm) {
+  _handleLogin(event, serializedForm) {
     event.preventDefault()
     this.setState({isSubmitting: true})
     login(serializedForm.email, serializedForm.password)
@@ -25,19 +23,30 @@ import { login } from '../../services/self'
   }
 
   render() {
+    const { isOpen } = this.state
+
     return this.state.signedIn ? <Redirect to='/apis' /> : (
-      <Modal size='small' trigger={<Button primary fluid>Sign In</Button>}>
+      <Modal
+        size='small'
+        open={isOpen}
+        onOpen={this.open}
+        onClose={this.close}
+        trigger={<Button primary fluid>Sign In</Button>}
+      >
         <Modal.Header>Sign in</Modal.Header>
         <Modal.Content>
           <Modal.Description>
             <Form onSubmit={this.handleLogin} error={!!this.state.errorMessage} loading={this.state.isSubmitting}>
               <Form.Input label='Email' name='email' />
               <Form.Input type='password' label='Password' name='password' autoComplete='false' />
-              <Button primary type='submit'>Sign In</Button>
               <Message error content={this.state.errorMessage} />
             </Form>
           </Modal.Description>
         </Modal.Content>
+        <Modal.Actions>
+          <Button onClick={this.close}>Close</Button>
+          <Button primary onClick={this.handleLogin}>Sign In</Button>
+        </Modal.Actions>
       </Modal>)
   }
 }
