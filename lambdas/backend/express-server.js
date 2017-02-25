90e7634948253bee9925d571edf4971a0ea48c3c
Morgan Catlin <morgan.catlin@valtira.net>
2017-02-25 17:23:41 -0600

    Subscribes customer to Usage Plan
    Subscribes customer to Usage Plan after they confirm the Marketplace Subscription.

--- lambdas/backend/express-server.js
+++ lambdas/backend/express-server.js
@@ -243,6 +243,9 @@ app.put('/marketplace-subscriptions/:usagePlanId', (req, res) => {
             // this is used when the subscription listener receives the subscribe notification
             const marketplaceCustomerId = data.CustomerIdentifier
             customersController.updateCustomerMarketplaceId(cognitoIdentityId, marketplaceCustomerId, error, success)
+            
+            // also, subscribe the customer to the usage plan!
+            customersController.subscribe(cognitoIdentityId, usagePlanId, error, success)
         }
     })
 })
