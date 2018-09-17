bad511ce1733fe9729e92ab797079fb99c801c0f
Ashton Eby <ebashton@amazon.com>
2018-09-17 13:26:44 -0700

    Add catalog updater lambda
    When a user uploads or deletes a file with a key starting with
    "catalog/" from the artifacts bucket, this newly created lambda is
    triggered. The lambda generates catalog.json and stores it in the
    artifacts bucket. This change also introduces a change to the backend
    lambda to look the catalog file up from S3 instead of having it deployed
    inside the lambda function.

---
+++ lambdas/catalog-updater/index.js
@@ -0,0 +1,142 @@
+'use strict';
+
+// TODO: This whole lambda may need to be 'debounced' in the future via SNS/SQS
+// Right now it runs once per file, and thus may have nasty race conditions
+
+let AWS = require('aws-sdk'),
+  s3 = new AWS.S3(),
+  gateway = new AWS.APIGateway(),
+  _ = require('lodash'),
+  bucketName = ''
+
+/**
+ * Takes in an s3 listObjectsV2 object and returns whether or not it's a swagger or yaml file.
+ *
+ * @param {Object} file an s3 listObjectsV2 response object
+ * @returns {boolean} whether or not the file is a swagger or yaml file
+ */
+function swaggerFileFilter(file) {
+  let extension = file.Key.split('/').pop().split('.').pop(),
+    isSwagger = (extension === 'json' || extension === 'yaml' || extension === 'yml')
+  console.log(`file ${file.Key} is${isSwagger? '': ' not'} a swagger file`)
+  return isSwagger
+}
+
+function getSwaggerFile(file) {
+  let params = {
+    Bucket: bucketName,
+    Key: file.Key
+  }
+
+  return s3.getObject(params).promise()
+    .then((obj) => {
+      console.log(`swaggerFile: ${JSON.stringify(obj)}`)
+      return obj
+    })
+    .catch((error) => {
+      console.log(`error retrieving swagger file ${file.Key}: ${error}`)
+    })
+}
+
+/**
+ *
+ *
+ * Example catalog object:
+ * {
+ *   id: 'YOUR_USAGE_PLAN_ID',
+ *   name: 'Free',
+ *   apis: [{
+ *     id: 'YOUR_API_ID',
+ *     image: '/sam-logo.png',
+ *     swagger: petStoreSwaggerDefinition
+ *   }]
+ * }
+ * @param {Object} usagePlan the API GW CS representation of a usage plan
+ * @param {Array} swaggerFiles array of swagger file bodies retrieved from s3
+ * @returns {Object} a 'catalog object' with id, name, and apis properties
+ */
+function usagePlanToCatalogObject(usagePlan, swaggerFiles) {
+  let catalogObject = {
+    id: usagePlan.id,
+    name: usagePlan.name,
+    //TO-DO: Allow for customizing image?
+    apis: _.map(usagePlan.apiStages, (apiStage) => ({ id: usagePlan.apiId, image: '/sam-logo.png' }))
+  }
+
+  catalogObject.apis = _.map(catalogObject.apis, (api) => {
+
+    api.swagger = _.filter(swaggerFiles, () => {
+      return swaggerFiles.indexOf(api.id)
+    })
+
+    return api
+  })
+
+  return catalogObject
+}
+
+exports.handler = (event, context) => {
+  try {
+    console.log(`event: ${JSON.stringify(event, null, 4)}`)
+    // this is really fragile
+    bucketName = _.get(event, 'Records[0].s3.bucket.name')
+    let params = {
+      Bucket: bucketName
+    },
+    promises = []
+
+    return s3.listObjectsV2(params).promise()
+      .then((result) => {
+        console.log(`result: ${JSON.stringify(result, null, 4)}`)
+        let promises =
+          _
+          .chain(result.Contents)
+          .filter(swaggerFileFilter)
+          .map(getSwaggerFile)
+          .value()
+
+        console.log(`promises: ${JSON.stringify(promises)}`)
+        return Promise.all(promises)
+      })
+      .then((swaggerFiles) => {
+        console.log(`results: ${JSON.stringify(swaggerFiles, null, 4)}`)
+        let catalogObjects = []
+
+        swaggerFiles.forEach((file, index) => {
+          swaggerFiles[index] = file.Body.toString()
+          console.log(`swaggerFile #${index+1}: ${swaggerFiles[index].substring(0, 100)}`)
+        })
+
+        return gateway.getUsagePlans({}).promise()
+          .then((result) => {
+            console.log(`usagePlans: ${JSON.stringify(result.items, null, 4)}`)
+            let usagePlans = result.items
+            for (let i = 0; i < usagePlans.length; i++) {
+              catalogObjects[i] = usagePlanToCatalogObject(usagePlans[i], swaggerFiles)
+            }
+            console.log(`catalogObjects: ${JSON.stringify(catalogObjects, null, 4)}`)
+
+            return catalogObjects
+          })
+          .catch((error) => console.log(`error getting usage plans: ${error}`))
+
+      })
+      .then((catalogObjects) => {
+        let params = {
+            Bucket: bucketName,
+            Key: 'catalog.json',
+            Body: JSON.stringify(catalogObjects),
+            ContentType: 'application/json'
+          },
+          options = {}
+        return s3.upload(params, options).promise()
+          .then((response) => console.log(`s3 upload succeeded: ${JSON.stringify(response, null, 4)}`))
+          .catch((error) => console.log(`error uploading catalog to s3: ${error}`))
+      })
+      .catch((error) => {
+        console.log(`error operating on bucket ${bucketName}: ${error}`)
+      })
+  } catch (error) {
+    console.log(`Caught top-level error: ${error}`)
+  }
+}
