c12ecbb291bf1140f4c0986ff5a3ce5931ace5ff
Nathaniel Hutchins <nhutch@amazon.com>
2018-10-02 16:11:36 -0700

    fixing sign in and sign up; added safeguards against new window links

--- dev-portal/src/pages/CaseStudies/index.js
+++ dev-portal/src/pages/CaseStudies/index.js
@@ -10,5 +10,5 @@ export default (props) => (
     <p>Build your own local PetStore by reselling our pets. Use our APIs to check availability data and fulfill orders.</p>
     <h3>Sell us your Pets</h3>
     <p>Establish yourself as our partner and get access to our POST API – to directly post pets to our inventory.</p>
-    <p>Successfully built an app? <a href='http://docs.aws.amazon.com/apigateway/latest/developerguide/sell-api-as-saas-on-aws-marketplace.html' target='_blank'>Register as a seller in AWS Marketplace</a> and sell your application! Questions or comments? <a href='https://forums.aws.amazon.com/forum.jspa?forumID=199' target='_blank'>Reach out to us on our forums</a>.</p>
+    <p>Successfully built an app? <a href='http://docs.aws.amazon.com/apigateway/latest/developerguide/sell-api-as-saas-on-aws-marketplace.html' target='_blank' rel="noopener noreferrer">Register as a seller in AWS Marketplace</a> and sell your application! Questions or comments? <a href='https://forums.aws.amazon.com/forum.jspa?forumID=199' target='_blank' rel="noopener noreferrer">Reach out to us on our forums</a>.</p>
   </div>)
