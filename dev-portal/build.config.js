44ecdfa6d870fcdd9cc354baa50b243d28fca95a
Nathaniel Hutchins <nhutch@amazon.com>
2018-10-02 18:28:49 -0700

    add auto-build script and build artifacts

---
+++ dev-portal/build.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  stackName: `nathaniels-dev-portal`,
+  buildAssetsBucket: `nathaniels-dev-portal-build-assets`,
+  siteAssetsBucket: `nathaniels-dev-portal-site-assets`,
+  apiAssetsBucket: `nathaniels-dev-portal-api-assets`
+}
--- dev-portal/public/config.js
+++ dev-portal/public/config.js
@@ -1,7 +1,7 @@
 window.config = {
-  region: 'us-east-1',
-  restApiId: 'srn0kpsb0f',
-  userPoolId: 'us-east-1_XaM1jwmbL',
-  identityPoolId: 'us-east-1:8357fff2-858b-4e55-8a97-ae6084a8d628',
-  userPoolClientId: '75qpr1vpif2i3sv08vv95nquqc'
+  "identityPoolId": "us-east-1:8357fff2-858b-4e55-8a97-ae6084a8d628",
+  "userPoolClientId": "75qpr1vpif2i3sv08vv95nquqc",
+  "userPoolId": "us-east-1_XaM1jwmbL",
+  "restApiId": "srn0kpsb0f",
+  "region": "us-east-1"
 }
