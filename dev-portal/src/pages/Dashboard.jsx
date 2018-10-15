1a19b02eeaf9c3c854758ed4616595f868c54be2
Nathaniel Hutchins <nhutch@amazon.com>
2018-10-15 08:33:23 -0700

    Updating getting started and dashboard pages

--- dev-portal/src/pages/Dashboard.jsx
+++ dev-portal/src/pages/Dashboard.jsx
@@ -1,3 +1,91 @@
 import React from 'react'
 
-export default () => (<p>Dashboard</p>)
+import { Grid, Header, Segment, Loader, Message } from 'semantic-ui-react'
+
+import Chart from 'chart.js'
+import { fetchUsage, mapUsageByDate } from 'services/api-catalog'
+
+import { observer } from 'mobx-react'
+
+import { store } from 'services/state'
+
+import _ from 'lodash'
+
+function loadUsage(usagePlan, canvasId) {
+
+  console.log(React.Component)
+  fetchUsage(usagePlan.id)
+    .then((result) => {
+      const data = mapUsageByDate(result.data, 'used')
+      const ctx = document.getElementById(canvasId)
+
+      let oldDataString = JSON.stringify(_.get(usagePlan, 'usage.data', {}))
+      let newDataString = JSON.stringify(data)
+
+      if (oldDataString !== newDataString)
+        usagePlan.usage = { data }
+
+      const chartConfig = {
+        type: 'line',
+        data: {
+          labels: data.map(d => new Date(parseInt(d[0], 10)).toLocaleDateString()),
+          datasets: [
+            {
+              label: 'Usage',
+              data: data.map(d => d[1]),
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+              borderColor: 'rgba(255,99,132,1)',
+              borderWidth: 1
+            }
+          ]
+        },
+        options: { scales: { yAxes: [ { ticks: { beginAtZero: true } } ] } }
+      }
+
+      new Chart(ctx, chartConfig)
+    })
+    .catch((error) => {
+      usagePlan.usage = { error }
+    })
+}
+
+export default observer(() => {
+
+  return (
+    <Grid>
+      {store.catalog
+        .filter(usagePlan => usagePlan.subscribed)
+        .map(usagePlan => {
+
+        let apis = usagePlan.apis.reduce((acc, api) => {
+          return acc + ' ' + api.swagger.info.title
+        }, '')
+
+        let canvasId = `api-usage-chart-container-${usagePlan.id}`
+
+        console.log('render')
+
+        loadUsage(usagePlan, canvasId)
+
+        return (
+          <Grid.Column mobile={16} tablet={8} computer={4} key={usagePlan.id}>
+            <Segment>
+              <Header size="medium">
+                {usagePlan.name}
+              </Header>
+              <Header size="small">For APIs {apis}</Header>
+              { !usagePlan.usage ? (
+                <Loader active />
+              ) : ( 
+                usagePlan.error ? (
+                  <Message error content={usagePlan.error.toString()} />
+                ) : null
+              )}
+              <canvas id={canvasId} width='400' height='400' />
+            </Segment>
+          </Grid.Column>
+        )
+      })}
+    </Grid>
+  )
+})
--- dev-portal/src/pages/GettingStarted.jsx
+++ dev-portal/src/pages/GettingStarted.jsx
@@ -1,17 +1,67 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
+import { Container, Header, Menu, Segment } from 'semantic-ui-react'
+
 export default (props) => (
-  <div>
-    <h2>Getting Started</h2>
-    <p>Sample data. You can modify this static content in `/dev-portal/src/pages/GettingStarted/index.js` folder.</p>
-    <h3>Get Started with our PetStore API</h3>
-    <ol style={{listStyle: 'decimal', paddingLeft: '1.5rem', marginBottom: '1rem'}}>
-      <li>Set up a developer account. Developer accounts here are managed by <a href='http://docs.aws.amazon.com/cognito/latest/developerguide/cognito-user-identity-pools.html' target='_blank' rel="noopener noreferrer">AWS Cognito User Pools.</a></li>
-      <li>Subscribe to an API and get an <a href='https://docs.aws.amazon.com/apigateway/api-reference/resource/api-key/' target='_blank' rel="noopener noreferrer">API Key</a>. Once you get it, you can use it for all the API products published here.</li>
-      <li>Build your application. Use the x-api-key header to send the API key in your requests</li>
-    </ol>
-    <p>The API plans that you subscribe to are governed by throttling limits and quotas as described in the API description.
-    If you have exhausted your limits and want temporary extensions, contact us.</p>
-    <p>Successfully built an app? <a href='http://docs.aws.amazon.com/apigateway/latest/developerguide/sell-api-as-saas-on-aws-marketplace.html' target='_blank' rel="noopener noreferrer">Register as a seller</a> in AWS Marketplace and sell your application!
-    Questions or comments? Reach out to us on our <a href='https://forums.aws.amazon.com/forum.jspa?forumID=199' target='_blank' rel="noopener noreferrer">forums</a>.</p>
-  </div>)
+  <div style={{ display: "flex", flex: "1 1 auto", overflow: "hidden" }}>
+    <Menu
+      inverted
+      vertical
+      attached
+      style={{ margin: 0, borderRadius: 0 }}
+      {...props}
+    >
+      <Menu.Item
+        as={Link}
+        to={`#part`}
+      >
+        Getting Started
+      </Menu.Item>
+    </Menu>
+
+    <div style={{ padding: "40px", overflow: "auto" }}>
+      <Header as='h1'>Getting Started</Header>
+
+      <Segment vertical>
+        <Header as="h2">Create an Account</Header>
+        <p>
+          Create an account and get an <a href='https://docs.aws.amazon.com/apigateway/api-reference/resource/api-key/' target='_blank' rel="noopener noreferrer">API Key</a>.
+        </p>
+      </Segment>
+      <Segment vertical>
+        <Header as="h2">View and Subscribe to our APIs</Header>
+        <p>
+          View our APIs here. Once you find what you need, subscribe to the API to activate your API key for that API.
+        </p>
+        <p>
+          This section might be combined with the above section.
+        </p>
+      </Segment>
+      <Segment vertical>
+        <Header as="h2">Build an App</Header>
+
+        <p>This part of the document should have code samples on how to integrate here. If we provide a hook to download the SDKs, we should point that out here.</p>
+        <p>Build your application. Use the x-api-key header to send the API key in your requests.</p>
+
+      </Segment>
+      <Segment vertical>
+        <Header as="h2">Next Steps</Header>
+
+        <Header as="h3">Throttling Limits</Header>
+
+        <p>What do do if you hit throttling limits.</p>
+        <p>The API plans that you subscribe to are governed by throttling limits and quotas as described in the API description. If you have exhausted your limits and want temporary extensions, contact us.</p>
+
+        <Header as="h3">Built an App?</Header>
+        <p>Successfully built an app? <a href='http://docs.aws.amazon.com/apigateway/latest/developerguide/sell-api-as-saas-on-aws-marketplace.html' target='_blank' rel="noopener noreferrer">Register as a seller</a> in AWS Marketplace and sell your application!</p>
+
+        <Header>Questions?</Header>
+        <p>
+          Questions or comments? Reach out to us on our <a href='https://forums.aws.amazon.com/forum.jspa?forumID=199' target='_blank' rel="noopener noreferrer">forums</a>.
+        </p>
+      </Segment>
+    </div>
+  </div>
+)
