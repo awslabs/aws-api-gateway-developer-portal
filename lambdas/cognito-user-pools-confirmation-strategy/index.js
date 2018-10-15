eaf729b9168ff01ce524fddd508930d91ebde1a5
Ashton Eby <ebashton@amazon.com>
2018-10-15 16:21:52 -0700

    Document & format confirmation strategy lambda

--- lambdas/cognito-user-pools-confirmation-strategy/index.js
+++ lambdas/cognito-user-pools-confirmation-strategy/index.js
@@ -1,1 +1,7 @@
-exports.handler = (event, context, callback)  => { event.response = { autoConfirmUser: true }; callback(null, event); }
+// This lambda function is attached to the "pre-sign-up" cognito hook
+// It determines if a user should be added to the user pool
+// Right now, it always returns true.
+
+exports.handler = (event, context, callback) => {
+    event.response = { autoConfirmUser: true }; callback(null, event);
+}
