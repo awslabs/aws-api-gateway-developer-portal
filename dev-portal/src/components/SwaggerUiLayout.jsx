6d0c0cf72f95c5eff1c53e58352cd0fcac6a1bba
Nathaniel Hutchins <nhutch@amazon.com>
2018-10-26 15:30:02 -0700

    minor bug fixes
    - fixed spinner of death if there's no catalog.json
    - fixed images not reloading if src changes
    - fixed images not loading default if being redirected

--- dev-portal/src/components/SwaggerUiLayout.jsx
+++ dev-portal/src/components/SwaggerUiLayout.jsx
@@ -17,7 +17,7 @@ export const SwaggerLayoutPlugin = () => ({ components: { InfoContainer: InfoRep
 // replaces the InfoContainer component
 // https://github.com/swagger-api/swagger-ui/blob/dd3afdc45656bda2a64ae6a7f9bdad006ea98149/src/core/components/layouts/base.jsx
 
-const InfoReplacement = ({ specSelectors }) => {
+const InfoReplacement = observer(({ specSelectors }) => {
   const basePath = specSelectors.basePath()
   const host = specSelectors.host()
   const externalDocs = specSelectors.externalDocs()
@@ -48,7 +48,7 @@ const InfoReplacement = ({ specSelectors }) => {
       </div>
     </Container>
   )
-}
+})
 
 const SubscriptionButtons = observer(class SubscriptionButtons extends React.Component {
   state = {}
