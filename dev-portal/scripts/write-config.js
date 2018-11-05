ce9d3cb42158c0c3239778c0b065447c441b3b4a
Nitish Dayal <nitdayal@amazon.com>
2018-11-05 14:58:28 -0800

    fix(Generic Swagger, write-config)
    - Move conditional check for generic API to top of if/else block
    - write-config was broken due to L32; slight refactor

--- dev-portal/scripts/write-config.js
+++ dev-portal/scripts/write-config.js
@@ -7,33 +7,35 @@ const writeFile = util.promisify(require('fs').writeFile)
 const { execute, r } = require('./utils.js')
 
 const buildConfig = require('../deployer.config.js')
-let stackName = buildConfig.stackName
+const stackName = buildConfig.stackName
 
 function writeConfig (swallowOutput) {
   return execute(`aws cloudformation describe-stack-resources --stack-name ${stackName}`, swallowOutput)
   .then((result) => {
-    let configData = {}
+    const configData = {}
+    const responseToJSON = JSON.parse(result.stdout)
+    const resources = responseToJSON.StackResources
 
-    JSON.parse(result.stdout).StackResources.forEach(output => {
-      switch (output.LogicalResourceId) {
+    configData.region = resources[0].StackId.split(':')[3]
+
+    resources.forEach(resource => {
+      switch (resource.LogicalResourceId) {
         case 'ApiGatewayApi':
-          return configData.restApiId = output.PhysicalResourceId
+          return configData.restApiId = resource.PhysicalResourceId
         case 'CognitoUserPool':
-          return configData.userPoolId = output.PhysicalResourceId
+          return configData.userPoolId = resource.PhysicalResourceId
         case 'CognitoIdentityPool':
-          return configData.identityPoolId = output.PhysicalResourceId
+          return configData.identityPoolId = resource.PhysicalResourceId
         case 'CognitoUserPoolClient':
-          return configData.userPoolClientId = output.PhysicalResourceId
+          return configData.userPoolClientId = resource.PhysicalResourceId
         default:
           return
         }
     })
 
-    configData.region = outputs[0].StackId.split(':')[3]
-
     if(!swallowOutput) console.log(JSON.stringify(configData, null, 4))
 
-    let configFile = `window.config = ${JSON.stringify(configData, null, 2)}`
+    const configFile = `window.config = ${JSON.stringify(configData, null, 2)}`
     return writeFile(r(`../public/config.js`), configFile)
   })
   .catch((err) => {
