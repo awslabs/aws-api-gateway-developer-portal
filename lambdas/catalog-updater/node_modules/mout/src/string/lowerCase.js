75374b4687c0eb889ae6a63a43f57b83bfe712f4
Nathaniel Hutchins <nhutch@amazon.com>
2018-11-12 15:30:35 -0800

    update built assets and versions for release

--- lambdas/catalog-updater/node_modules/mout/src/string/lowerCase.js
+++
@@ -1,9 +0,0 @@
-define(function(){
-    /**
-     * "Safer" String.toLowerCase()
-     */
-    function upperCase(str){
-        return (str || '').toLowerCase();
-    }
-    return upperCase;
-});
--- lambdas/listener/node_modules/mout/src/string/pascalCase.js
+++
@@ -1,9 +0,0 @@
-define(['./camelCase', './upperCase'], function(camelCase, upperCase){
-    /**
-     * camelCase + uppercase first char
-     */
-    function pascalCase(str){
-        return camelCase(str).replace(/^[a-z]/, upperCase);
-    }
-    return pascalCase;
-});
