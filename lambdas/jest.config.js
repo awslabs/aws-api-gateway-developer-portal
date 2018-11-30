0f267c846375a6bdf13c791ac443d696bf7e7f38
Ashton Eby <ebashton@amazon.com>
2018-11-30 08:55:58 -0800

    Disable console.log in all lambdas during tests
    Previously, console.log was disabled per-test; we shouldn't need that
    output at all during test runs. This disables it globally (and also
    provides a file, `setup-jest.js` for building hooks for any other
    behavior we want globally!)

--- lambdas/jest.config.js
+++ lambdas/jest.config.js
@@ -116,7 +116,7 @@ module.exports = {
   // setupFiles: [],
 
   // The path to a module that runs some code to configure or set up the testing framework before each test
-  // setupTestFrameworkScriptFile: null,
+  setupTestFrameworkScriptFile: "<rootDir>/setup-jest.js",
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
