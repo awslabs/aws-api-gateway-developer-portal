17f2281c3c519d545e1b4f5fdcb1eb88835e06e0
Ashton Eby <ebashton@amazon.com>
2018-12-04 15:41:22 -0800

    Update built assets for v2.2.4

--- lambdas/static-asset-uploader/build/service-worker.js
+++ lambdas/static-asset-uploader/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5d7092a0932bde1757ea3eb49d95d8f.js"
+  "/precache-manifest.66c77ef37ea13502b28893efec198bb7.js"
 );
 
 workbox.clientsClaim();
