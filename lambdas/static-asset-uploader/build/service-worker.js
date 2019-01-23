3b265158de8618053c9a91c20f6fc8f1f17b723f
Ashton Eby <ebashton@amazon.com>
2019-01-22 17:11:04 -0800

    Update static assets for v2.3.2

--- lambdas/static-asset-uploader/build/service-worker.js
+++ lambdas/static-asset-uploader/build/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.000a4b73ba097441b941c1b3250e43c7.js"
+  "/precache-manifest.20b0f3558c7d65be0353cd7bf0ad9e0b.js"
 );
 
 workbox.clientsClaim();
