3299f9fa8a44095b4636928bea0b94b9a90d99f7
Ashton Eby <ebashton@amazon.com>
2019-02-07 16:36:17 -0800

    Remove Identity Pool and related resources
    Moving to OAuth 2 / scopes will require tearing out the identity pool,
    identity pool roles, and authenticated role. Additionally, this commit
    cleans up the names of some of the user pool comments / variables that
    were erroneously named after the identity pool resource.

--- dev-portal/src/services/api.js
+++ dev-portal/src/services/api.js
@@ -6,7 +6,6 @@ import AWS from 'aws-sdk'
 export const awsRegion = window.config.region
 export const cognitoRegion = window.config.region
 export const cognitoUserPoolId = window.config.userPoolId
-export const cognitoIdentityPoolId = window.config.identityPoolId
 export const cognitoClientId = window.config.userPoolClientId
 export const cognitoDomain = window.config.userPoolDomain
 
