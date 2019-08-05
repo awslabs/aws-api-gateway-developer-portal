cb02023c9c2134d025caca1668edb7eb0cd10653
Alex Chew <chewalex@amazon.com>
2019-08-05 15:19:24 -0700

    AccountsTestUtils: add docs

--- dev-portal/src/utils/AccountsTestUtils.jsx
+++ dev-portal/src/utils/AccountsTestUtils.jsx
@@ -4,11 +4,31 @@ import * as rtl from '@testing-library/react'
 import * as AccountsTable from 'components/Admin/Accounts/AccountsTable'
 import * as AccountsTableColumns from 'components/Admin/Accounts/AccountsTableColumns'
 
-export const waitForAccountsToLoad = page =>
+/**
+ * A promise that waits for `AccountsTable.LoadingAccountRow` elements to be
+ * removed. Has the same operating restriction as `waitForElementToBeRemoved`;
+ * namely, that such an element must be present when this function is first
+ * called.
+ *
+ * @param container
+ *    the element in which to search for `LoadingAccountRow` elements
+ */
+export const waitForAccountsToLoad = container =>
   rtl.waitForElementToBeRemoved(() =>
-    page.queryAllByTestId(AccountsTable.ACCOUNT_ROW_PLACEHOLDER_TESTID),
+    container.queryAllByTestId(AccountsTable.ACCOUNT_ROW_PLACEHOLDER_TESTID),
   )
 
+/**
+ * Like a `queryAll` from RTL, but searches within the `container` element for
+ * `text` within the column identified by `columnId`.
+ *
+ * @param container
+ *    the element in which to search for `text`
+ * @param columnId
+ *    the `id` of a column descriptor (see `AccountsTableColumns`)
+ * @param text
+ *    the `TextMatch` for which to search
+ */
 export const queryAllByColumnText = (container, columnId, text) =>
   rtl
     .queryAllByText(container, text)
@@ -17,9 +37,22 @@ export const queryAllByColumnText = (container, columnId, text) =>
         el.getAttribute(AccountsTable.ACCOUNT_COLUMN_ID_DATA_ATTR) === columnId,
     )
 
+/**
+ * Like `queryAllByColumnText`, but returns only one element if found, or
+ * `null` if no elements are found.
+ */
 export const queryByColumnText = (container, columnId, text) =>
   _.get(queryAllByColumnText(container, columnId, text), [0], null)
 
+/**
+ * Expects that `email` appears in the `EmailAddress` column of the
+ * `container`.
+ *
+ * @param email
+ *    the `TextMatch` representing an email address, for which to search
+ * @param container
+ *    the element in which to search
+ */
 export const expectEmailIn = (email, container) =>
   expect(
     queryByColumnText(container, AccountsTableColumns.EmailAddress.id, email),
