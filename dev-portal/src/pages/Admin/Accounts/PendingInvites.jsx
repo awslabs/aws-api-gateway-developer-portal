ae5f7faeecc06ae6c551ee33ce513cebffcb3ce2
Alex Chew <chewalex@amazon.com>
2019-08-27 11:17:17 -0700

    PendingInvites: remove unnecssary React.memo's

--- dev-portal/src/pages/Admin/Accounts/PendingInvites.jsx
+++ dev-portal/src/pages/Admin/Accounts/PendingInvites.jsx
@@ -144,17 +144,20 @@ const PendingInvites = () => {
 }
 export default PendingInvites
 
-const TableActions = React.memo(
-  ({ canCreate, onClickCreate, canDelete, onClickDelete }) => (
-    <Button.Group>
-      <Button
-        content='Create invite...'
-        disabled={!canCreate}
-        onClick={onClickCreate}
-      />
-      <Button content='Delete' disabled={!canDelete} onClick={onClickDelete} />
-    </Button.Group>
-  ),
+const TableActions = ({
+  canCreate,
+  onClickCreate,
+  canDelete,
+  onClickDelete,
+}) => (
+  <Button.Group>
+    <Button
+      content='Create invite...'
+      disabled={!canCreate}
+      onClick={onClickCreate}
+    />
+    <Button content='Delete' disabled={!canDelete} onClick={onClickDelete} />
+  </Button.Group>
 )
 
 /*
@@ -214,67 +217,60 @@ const CreateInviteModal = ({ onConfirm, open, onClose, messages }) => {
   )
 }
 
-const DeleteInviteModal = React.memo(
-  ({ account, onConfirm, open, onClose }) =>
-    account && (
-      <Modal size='small' open={open} onClose={onClose}>
-        <Modal.Header>Confirm invite deletion</Modal.Header>
-        <Modal.Content>
-          <p>
-            Are you sure you want to delete this account invite for{' '}
-            <strong>{account.emailAddress}</strong>? This action is
-            irreversible.
-          </p>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button onClick={onClose}>Cancel</Button>
-          <Button negative onClick={onConfirm}>
-            Delete
-          </Button>
-        </Modal.Actions>
-      </Modal>
-    ),
-)
+const DeleteInviteModal = ({ account, onConfirm, open, onClose }) =>
+  account ? (
+    <Modal size='small' open={open} onClose={onClose}>
+      <Modal.Header>Confirm invite deletion</Modal.Header>
+      <Modal.Content>
+        <p>
+          Are you sure you want to delete this account invite for{' '}
+          <strong>{account.emailAddress}</strong>? This action is irreversible.
+        </p>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button negative onClick={onConfirm}>
+          Delete
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  ) : null
 
-const CreateSuccessMessage = React.memo(({ emailAddress, dismiss }) => (
+const CreateSuccessMessage = ({ emailAddress, dismiss }) => (
   <Message onDismiss={dismiss} positive>
     <Message.Content>
       Sent account invite to <strong>{emailAddress}</strong>.
     </Message.Content>
   </Message>
-))
-
-const CreateFailureMessage = React.memo(
-  ({ emailAddress, errorMessage, dismiss }) => (
-    <Message onDismiss={dismiss} negative>
-      <Message.Content>
-        <p>
-          Failed to send account invite to <strong>{emailAddress}</strong>.
-        </p>
-        {errorMessage && <p>Error message: {errorMessage}</p>}
-      </Message.Content>
-    </Message>
-  ),
 )
 
-const DeleteSuccessMessage = React.memo(({ account, dismiss }) => (
+const CreateFailureMessage = ({ emailAddress, errorMessage, dismiss }) => (
+  <Message onDismiss={dismiss} negative>
+    <Message.Content>
+      <p>
+        Failed to send account invite to <strong>{emailAddress}</strong>.
+      </p>
+      {errorMessage && <p>Error message: {errorMessage}</p>}
+    </Message.Content>
+  </Message>
+)
+
+const DeleteSuccessMessage = ({ account, dismiss }) => (
   <Message onDismiss={dismiss} positive>
     <Message.Content>
       Deleted account invite for <strong>{account.emailAddress}</strong>.
     </Message.Content>
   </Message>
-))
+)
 
-const DeleteFailureMessage = React.memo(
-  ({ account, errorMessage, dismiss }) => (
-    <Message onDismiss={dismiss} negative>
-      <Message.Content>
-        <p>
-          Failed to delete account invite for{' '}
-          <strong>{account.emailAddress}</strong>.
-        </p>
-        {errorMessage && <p>Error message: {errorMessage}</p>}
-      </Message.Content>
-    </Message>
-  ),
+const DeleteFailureMessage = ({ account, errorMessage, dismiss }) => (
+  <Message onDismiss={dismiss} negative>
+    <Message.Content>
+      <p>
+        Failed to delete account invite for{' '}
+        <strong>{account.emailAddress}</strong>.
+      </p>
+      {errorMessage && <p>Error message: {errorMessage}</p>}
+    </Message.Content>
+  </Message>
 )
