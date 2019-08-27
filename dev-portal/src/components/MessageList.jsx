74b505c12a962a61e0092ad249ec31719b19e1c0
Alex Chew <chewalex@amazon.com>
2019-08-27 08:53:06 -0700

    MessageList: refactor to avoid components in state

--- dev-portal/src/components/MessageList.jsx
+++ dev-portal/src/components/MessageList.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 
 export const MessageList = ({ messages }) =>
-  messages.map((message, index) => (
-    <React.Fragment key={index}>{message}</React.Fragment>
+  messages.map(({ render, id }) => (
+    <React.Fragment key={id}>{render()}</React.Fragment>
   ))
 
 /**
  * A Hook for operating a list of "messages" which should be self-dismissable.
  * Returns `[messages, sendMessage]`, where:
- *    - `messages` is an array of renderable messages (of type `React.ReactNode`)
+ *    - `messages` is an array of messages, which should be provided as a prop
+ *      to `MessageList` to be rendered
  *    - `sendMessage` is a function which accepts a renderer callback, and
  *      calls the callback to obtain a renderable message to append to
  *      `messages`. The renderer callback should accept a `dismiss` function as
@@ -16,14 +17,28 @@ export const MessageList = ({ messages }) =>
  *      when called.
  */
 export const useMessages = () => {
-  const [messages, setMessages] = useState([])
+  const [state, setState] = useState({
+    messages: [],
+    nextId: 0,
+  })
 
   const sendMessage = renderWithDismiss => {
-    const target = renderWithDismiss(() => {
-      setMessages(messages => messages.filter(message => message !== target))
-    })
-    setMessages(messages => [...messages, target])
+    const id = state.nextId
+    const dismiss = () => {
+      setState(state => ({
+        ...state,
+        messages: state.messages.filter(message => message.id !== id),
+      }))
+    }
+    const newMessage = {
+      render: () => renderWithDismiss(dismiss),
+      id: state.nextId,
+    }
+    setState(state => ({
+      messages: [...state.messages, newMessage],
+      nextId: state.nextId + 1,
+    }))
   }
 
-  return [messages, sendMessage]
+  return [state.messages, sendMessage]
 }
