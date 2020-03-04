f7555df9ca851e6828295a772347b734ba24a440
amazon-meaisiah <meaisiah@amazon.com>
2020-03-04 13:23:29 -0800

    Increase limit to allow for larger API imports/etc.

--- lambdas/backend/index.js
+++ lambdas/backend/index.js
@@ -12,8 +12,8 @@ const awsServerlessExpress = require('aws-serverless-express')
 const app = express()
 
 app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ limit: '10mb' }))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 app.use(awsServerlessExpressMiddleware.eventContext())
 
 // user APIs
