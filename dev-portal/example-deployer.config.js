b177a9e0517bc2a27d469a67979b6668843110bf
amazon-meaisiah <meaisiah@amazon.com>
2020-06-08 11:05:18 -0700

    Forgot some line breaks.

--- dev-portal/example-deployer.config.js
+++ dev-portal/example-deployer.config.js
@@ -1,8 +1,13 @@
 // Here's how you set this up:
 // 
-// 1. Replace YOUR_LAMBDA_ARTIFACTS_BUCKET_NAME with the name of the bucket you created in step 3 of the dev setup.
-// 2. Replace 'CUSTOM_PREFIX' in the properties that have it with your name, your org name, or some other unique identifier. For the S3 buckets and the Cognito user pool domain prefix, they must be globally unique. For the CloudFormation stack name, it need only be unique to all stacks deployed to your account.
-// 3. Set any other optional parameters as desired. For the DynamoDB tables, their names must be unique to all DynamoDB tables within your account.
+// 1. Replace YOUR_LAMBDA_ARTIFACTS_BUCKET_NAME with the name of the bucket you created in step 3
+//    of the dev setup.
+// 2. Replace 'CUSTOM_PREFIX' in the properties that have it with your name, your org name, or some
+//    other unique identifier. For the S3 buckets and the Cognito user pool domain prefix, they
+//    must be globally unique. For the CloudFormation stack name, it need only be unique to all
+//    stacks deployed to your account.
+// 3. Set any other optional parameters as desired. For the DynamoDB tables, their names must be
+//    unique to all DynamoDB tables within your account.
 // 4. Save the file.
 // 
 // Note: these configuration parameters are *not* the same as the SAM template parameters - the names differ and the behavior in many areas also differ. Furthermore, some SAM template parameters like `StaticAssetsRebuildToken` are handled automatically internally and cannot be configured.
