4a7f479db9099a550e3592c1428beb90a5e03213
amazon-meaisiah <meaisiah@amazon.com>
2020-06-23 14:30:00 -0700

    Make much fuller

--- lambdas/cognito-user-migration-trigger/index.js
+++ lambdas/cognito-user-migration-trigger/index.js
@@ -1,14 +1,16 @@
 'use strict'
 
 const AWS = require('aws-sdk')
-const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({
+
+exports.cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({
   apiVersion: '2016-04-18'
 })
 
 exports.handler = async (event) => {
   let authReq
+
   if (event.triggerSource === 'UserMigration_Authentication') {
-    authReq = cognitoIdentityServiceProvider.adminInitiateAuth({
+    authReq = exports.cognitoIdentityServiceProvider.adminInitiateAuth({
       AuthFlow: 'ADMIN_USER_PASSWORD_AUTH',
       AuthParameters: {
         USERNAME: event.userName,
@@ -17,8 +19,30 @@ exports.handler = async (event) => {
       ClientId: event.callerContext.clientId,
       UserPoolId: event.userPoolId
     }).promise().then(resp => {
-      if (resp.code && resp.message) throw new Error('Bad password')
-      event.response.finalUserStatus = 'CONFIRMED'
+      if (resp.AuthenticationResult != null) {
+        event.response.finalUserStatus = 'CONFIRMED'
+      } else {
+        // Username exists, but for whatever reason, the password wasn't sufficient.
+        // Let's simplify and just force a password reset here.
+      }
+    }, e => {
+      // Mask the obvious errors.
+      if (
+        e.code === 'NotAuthorizedException' ||
+        e.code === 'UserLambdaValidationException' ||
+        e.code === 'UserNotConfirmedException' ||
+        e.code === 'UserNotFoundException' ||
+        e.code === 'MFAMethodNotFoundException'
+      ) {
+        throw new Error('Bad username or password')
+      }
+
+      // Let's go through the standard "forgot password" flow.
+      if (e.code === 'PasswordResetRequiredException') return
+      if (e.code === 'InvalidParameterException') throw new Error('Invalid parameter')
+      if (e.code === 'TooManyRequestsException') throw new Error('Too many requests')
+      console.error(e)
+      throw new Error('Internal error occurred.')
     })
   } else if (event.triggerSource === 'UserMigration_ForgotPassword') {
     // Non-error response required to enable password-reset code to be sent to user
@@ -27,19 +51,32 @@ exports.handler = async (event) => {
     throw new Error('Bad triggerSource ' + event.triggerSource)
   }
 
-  const [userInfoRequest] = await Promise.all([
-    cognitoIdentityServiceProvider.adminGetUser({
+  const [getUserResp] = await Promise.all([
+    exports.cognitoIdentityServiceProvider.adminGetUser({
       UserPoolId: event.userPoolId,
       Username: event.userName
-    }).promise().then(resp => {
-      if (resp.code && resp.message) throw new Error('Bad password')
+    }).promise().catch(e => {
+      // Mask the obvious errors.
+      if (
+        e.code === 'NotAuthorizedException' ||
+        e.code === 'UserNotFoundException'
+      ) {
+        throw new Error('Bad username or password')
+      }
+
+      if (e.code === 'InvalidParameterException') throw new Error('Invalid parameter')
+      if (e.code === 'TooManyRequestsException') throw new Error('Too many requests')
+      console.error(e)
+      throw new Error('Internal error occurred.')
     }),
     authReq
   ])
 
-  const email = userInfoRequest.UserAttributes.find(e => e.Name === 'email').Value
+  const email = getUserResp.UserAttributes.find(e => e.Name === 'email').Value
 
   event.response.userAttributes = { email, email_verified: 'true' }
   event.response.messageAction = 'SUPPRESS'
+  event.response.desiredDeliveryMediums = 'email'
+  event.response.forceAliasCreation = true
   return event
 }
