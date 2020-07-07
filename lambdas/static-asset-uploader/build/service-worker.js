396c862a94bb4da34c0c572f3f55faf80f391199
amazon-meaisiah <meaisiah@amazon.com>
2020-07-07 11:55:24 -0700

    v3.0.4

--- lambdas/static-asset-uploader/build/service-worker.js
+++ lambdas/static-asset-uploader/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a34f6f07da0e4c9969330f1c0db2bbca.js"
+  "/precache-manifest.102354ed64b61a60090ec8baf440c0b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
