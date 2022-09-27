a7ae1c5aaa043e7509af5888264b9c47ef99ce22
Andy Fegan <afegan@amazon.com>
2022-09-27 16:54:25 -0300

    Adding back static files (#587)

---
+++ lambdas/static-asset-uploader/build/config.js
@@ -0,0 +1,1 @@
+window.config={"restApiId":"x79vbb8g24","region":"us-east-1","identityPoolId":"us-east-1:a0e8ca1f-203f-4d2e-a202-3c318593369b","userPoolId":"us-east-1_UEutkA5dK","userPoolClientId":"1eolvme2untq94e8rceco899uh","userPoolDomain":"https://afegan-win-test-auth1.auth.us-east-1.amazoncognito.com","feedbackEnabled":false}
